e34c3d657fb9236b2273f60fab135f0d395d15e2
Christian Emmanuel Yañez Gonzalez <laithg0279@gmail.com>
2021-11-11 16:40:05 -0700

    creacionObjetos

---
+++ src/Model/Cuadruplos_1.java
@@ -0,0 +1,63 @@
+package Model;
+
+/**
+ *
+ * @author Gonza
+ */
+public class Cuadruplos_1 {
+
+    private String etiqueta;
+    private String Accion;
+    private String Arg1;
+    private String Arg2;
+    private String Resultado;
+
+    public Cuadruplos_1() {
+        this.etiqueta = "";
+        this.Accion = "";
+        this.Arg1 = "";
+        this.Arg2 = "";
+        this.Resultado = "";
+    }
+
+    public String getEtiqueta() {
+        return etiqueta;
+    }
+
+    public void setEtiqueta(String etiqueta) {
+        this.etiqueta = etiqueta;
+    }
+
+    public String getAccion() {
+        return Accion;
+    }
+
+    public void setAccion(String Accion) {
+        this.Accion = Accion;
+    }
+
+    public String getArg1() {
+        return Arg1;
+    }
+
+    public void setArg1(String Arg1) {
+        this.Arg1 = Arg1;
+    }
+
+    public String getArg2() {
+        return Arg2;
+    }
+
+    public void setArg2(String Arg2) {
+        this.Arg2 = Arg2;
+    }
+
+    public String getResultado() {
+        return Resultado;
+    }
+
+    public void setResultado(String Resultado) {
+        this.Resultado = Resultado;
+    }
+
+}
---
+++ src/Model/Cuadruplos_Contadores.java
@@ -0,0 +1,253 @@
+package Model;
+
+/**
+ *
+ * @author Gonza
+ */
+public class Cuadruplos_Contadores {
+    
+    private int amb;
+    private int TE;
+    private int TR;
+    private int TS;
+    private int TCH;
+    private int TEX;
+    private int TB;
+    private int TRX;
+    private int Arr;
+    private int call;
+    private int igual;
+    private int OpRel;
+    private int OperLog;
+    private int OperArit;
+    private int OperUn;
+    private int JF;
+    private int JMP;
+    private int valor;
+    private int ifE;
+    private int SWE;
+    private int ForE;
+    private int WhiE;
+    private int DEF;
+    private int PPALL;
+
+    public Cuadruplos_Contadores() {
+        this.amb = 0;
+        this.TE = 0;
+        this.TR = 0;
+        this.TS = 0;
+        this.TCH = 0;
+        this.TEX = 0;
+        this.TB = 0;
+        this.TRX = 0;
+        this.Arr = 0;
+        this.call = 0;
+        this.igual = 0;
+        this.OpRel = 0;
+        this.OperLog = 0;
+        this.OperArit = 0;
+        this.OperUn = 0;
+        this.JF = 0;
+        this.JMP = 0;
+        this.valor = 0;
+        this.ifE = 0;
+        this.SWE = 0;
+        this.ForE = 0;
+        this.WhiE = 0;
+        this.DEF = 0;
+        this.PPALL = 0;
+    }
+
+    public int getAmb() {
+        return amb;
+    }
+
+    public void setAmb() {
+        this.amb +=1;
+    }
+
+    public int getTE() {
+        return TE;
+    }
+
+    public void setTE() {
+        this.TE +=1;
+    }
+
+    public int getTR() {
+        return TR;
+    }
+
+    public void setTR() {
+        this.TR +=1;
+    }
+
+    public int getTS() {
+        return TS;
+    }
+
+    public void setTS() {
+        this.TS +=1;
+    }
+
+    public int getTCH() {
+        return TCH;
+    }
+
+    public void setTCH() {
+        this.TCH +=1;
+    }
+
+    public int getTEX() {
+        return TEX;
+    }
+
+    public void setTEX() {
+        this.TEX +=1;
+    }
+
+    public int getTB() {
+        return TB;
+    }
+
+    public void setTB() {
+        this.TB +=1;
+    }
+
+    public int getTRX() {
+        return TRX;
+    }
+
+    public void setTRX() {
+        this.TRX +=1;
+    }
+
+    public int getArr() {
+        return Arr;
+    }
+
+    public void setArr() {
+        this.Arr +=1;
+    }
+
+    public int getCall() {
+        return call;
+    }
+
+    public void setCall() {
+        this.call +=1;
+    }
+
+    public int getIgual() {
+        return igual;
+    }
+
+    public void setIgual() {
+        this.igual +=1;
+    }
+
+    public int getOpRel() {
+        return OpRel;
+    }
+
+    public void setOpRel() {
+        this.OpRel +=1;
+    }
+
+    public int getOperLog() {
+        return OperLog;
+    }
+
+    public void setOperLog() {
+        this.OperLog +=1;
+    }
+
+    public int getOperArit() {
+        return OperArit;
+    }
+
+    public void setOperArit() {
+        this.OperArit +=1;
+    }
+
+    public int getOperUn() {
+        return OperUn;
+    }
+
+    public void setOperUn() {
+        this.OperUn +=1;
+    }
+
+    public int getJF() {
+        return JF;
+    }
+
+    public void setJF() {
+        this.JF +=1;
+    }
+
+    public int getJMP() {
+        return JMP;
+    }
+
+    public void setJMP() {
+        this.JMP +=1;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor() {
+        this.valor +=1;
+    }
+
+    public int getIfE() {
+        return ifE;
+    }
+
+    public void setIfE() {
+        this.ifE +=1;
+    }
+
+    public int getSWE() {
+        return SWE;
+    }
+
+    public void setSWE() {
+        this.SWE +=1;
+    }
+
+    public int getForE() {
+        return ForE;
+    }
+
+    public void setForE() {
+        this.ForE +=1;
+    }
+
+    public int getWhiE() {
+        return WhiE;
+    }
+
+    public void setWhiE() {
+        this.WhiE +=1;
+    }
+
+    public int getDEF() {
+        return DEF;
+    }
+
+    public void setDEF() {
+        this.DEF +=1;
+    }
+
+    public int getPPALL() {
+        return PPALL;
+    }
+
+    public void setPPALL() {
+        this.PPALL +=1;
+    }
+
+}
